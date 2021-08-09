b3b19b28e340877a8c9567fce8baa2a22ac193fc
sarah <sarah001160@gmail.com>
2021-08-10 00:34:03 +0800

    finishedtictactoe

--- script.js
+++ script.js
@@ -12,9 +12,14 @@ const WINNING_COMBINATIONS=[
 ]
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winning-message');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
 let circleTurn
 
 startGame()
+
+restartButton.addEventListener('click',startGame)
 
 function startGame(){
     //從這邊開始繼續2021/8/1
@@ -22,21 +27,45 @@ function startGame(){
     //31:06秒開始閱讀
     circleTurn= false
     cellElements.forEach(cell =>{
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click',handleClick)
         cell.addEventListener('click',handleClick,{once:true})
     })
     setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
 }
 
 function handleClick(e){
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS: X_CLASS;
-    // if(checkWin(currentClass)){
-    //     console.log('winner')   
-    //    }
-    swapTurns()
-    setBoardHoverClass()
     placeMark(cell, currentClass)
+    if(checkWin(currentClass)){//若true就win了
+        console.log('winner'); 
+        endGame(false)  
+       }else if(isDraw()) {
+        endGame(true) 
+       } else{
+        swapTurns()//換邊
+        setBoardHoverClass() //灰色顯示
+       }   
 }
+
+function endGame(draw){//結束遊戲
+    if(draw){//是平局
+        winningMessageTextElement.innerText = "Draw!"
+    }else{//false的話,表示有一方贏了
+        winningMessageTextElement.innerText =`${circleTurn ? "O's" :"X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')//顯示文字
+}
+
+function isDraw() {//是平局
+   return [...cellElements].every(cell=>{
+       return cell.classList.contains(X_CLASS)||cell.classList.contains(CIRCLE_CLASS)
+   })
+}
+
 
 function placeMark(cell, currentClass){
     cell.classList.add(currentClass)
@@ -57,13 +86,13 @@ function setBoardHoverClass(){
     }
 }
 
-// function checkWin(currentClass){
-//     return WINNING_COMBINATIONS.some(combination =>{
-//         return combinatoin.every(index=>{
-//            return cellElement[index].classList.contains(currentClass);
-//        })
-//      })
-// }
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination =>{
+        return combination.every(index=>{
+           return cellElements[index].classList.contains(currentClass);
+       })
+     })
+}
 
 
 
